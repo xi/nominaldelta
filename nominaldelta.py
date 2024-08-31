c05b095fdf33b32fcfd455fd0996b4d9b7edfcae
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-08-31 02:42:24 +0200

    init

---
+++ nominaldelta.py
@@ -0,0 +1,150 @@
+from datetime import date
+from datetime import datetime
+from typing import Self
+
+
+def date_to_timestamp(d):
+    return datetime(d.year, d.month, d.day).timestamp()
+
+
+def date_add(dt, delta):
+    total_months = dt.year * 12 + dt.month + delta.months
+    year, month = divmod(total_months, 12)
+    if month == 0:
+        year -= 1
+        month = 12
+
+    # clip day to month
+    day = dt.day
+    while day > 0:
+        try:
+            tmp = date(year, month, day)
+            break
+        except ValueError:
+            day -= 1
+
+    return date.fromordinal(tmp.toordinal() + delta.days)
+
+
+def dt_add(dt, delta):
+    d = date_add(dt.date(), delta)
+    offset = dt.timestamp() - date_to_timestamp(dt.date())
+    return datetime.fromtimestamp(
+        date_to_timestamp(d) + offset + delta.seconds, tz=dt.tzinfo
+    )
+
+
+def binary_search(a, b, delta):
+    lower = 0
+    upper = 1
+    while a + delta * upper <= b:
+        lower = upper
+        upper <<= 1
+    while lower + 1 < upper:
+        tmp = (lower + upper) // 2
+        if a + delta * tmp <= b:
+            lower = tmp
+        else:
+            upper = tmp
+    return lower * delta
+
+
+def date_diff(a, b):
+    if a > b:
+        return -date_diff(b, a)
+    delta = binary_search(a, b, NominalDelta(months=1))
+    days = b.toordinal() - (a + delta).toordinal()
+    return delta + NominalDelta(days=days)
+
+
+def dt_diff(a, b):
+    delta = date_diff(a, b)
+    seconds = b.timestamp() - (a + delta).timestamp()
+    return delta + NominalDelta(seconds=seconds)
+
+
+class NominalDelta:
+    def __init__(
+        self: Self,
+        *,
+        years: int = 0,
+        months: int = 0,
+        weeks: int = 0,
+        days: int = 0,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: float = 0,
+    ):
+        self.months = years * 12 + months
+        self.days = weeks * 7 + days
+        self.seconds = hours * 3600 + minutes * 60 + seconds
+
+    def __repr__(self):
+        return (
+            f'NominalDelta(months={self.months}, days={self.days}, '
+            f'seconds={self.seconds})'
+        )
+
+    @property
+    def years(self: Self) -> int:
+        return self.months // 12
+
+    def __eq__(self, other: Self) -> Self:
+        if isinstance(other, NominalDelta):
+            return (
+                self.months == other.months
+                and self.days == other.days
+                and self.seconds == other.seconds
+            )
+        return NotImplemented
+
+    def __add__(self: Self, other: Self) -> Self:
+        if isinstance(other, NominalDelta):
+            return NominalDelta(
+                months=self.months + other.months,
+                days=self.days + other.days,
+                seconds=self.seconds + other.seconds,
+            )
+        return NotImplemented
+
+    def __sub__(self: Self, other: Self) -> Self:
+        if isinstance(other, NominalDelta):
+            return NominalDelta(
+                months=self.months - other.months,
+                days=self.days - other.days,
+                seconds=self.seconds - other.seconds,
+            )
+        return NotImplemented
+
+    def __neg__(self: Self) -> Self:
+        return NominalDelta() - self
+
+    def __mul__(self: Self, factor: int) -> Self:
+        if isinstance(factor, int):
+            return NominalDelta(
+                months=self.months * factor,
+                days=self.days * factor,
+                seconds=self.seconds * factor,
+            )
+        return NotImplemented
+
+    def __rmul__(self: Self, factor: int) -> Self:
+        return self * factor
+
+    def __radd__(self: Self, other: date) -> date:
+        if isinstance(other, datetime):
+            return dt_add(other, self)
+        elif isinstance(other, date):
+            return date_add(other, self)
+        return NotImplemented
+
+    def __rsub__(self: Self, other: date) -> date:
+        return (-self).__radd__(other)
+
+    @classmethod
+    def diff(cls, a: date, b: date):
+        if isinstance(a, datetime) and isinstance(b, datetime):
+            return dt_diff(a, b)
+        elif isinstance(a, date) and isinstance(b, date):
+            return date_diff(a, b)
+        raise TypeError('Unsupported types')
---
+++ test.py
@@ -0,0 +1,171 @@
+import unittest
+from datetime import date
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from nominaldelta import NominalDelta
+
+
+class TestNominalDelta(unittest.TestCase):
+    def test_years(self):
+        self.assertEqual(NominalDelta(years=3, months=15).years, 4)
+        self.assertEqual(NominalDelta(years=3, months=-1).years, 2)
+
+    def test_weeks(self):
+        self.assertEqual(NominalDelta(weeks=2, days=2).days, 16)
+        self.assertEqual(NominalDelta(weeks=2, days=-1).days, 13)
+
+    def test_add(self):
+        self.assertEqual(
+            NominalDelta(months=1) + NominalDelta(days=2),
+            NominalDelta(months=1, days=2),
+        )
+        self.assertEqual(
+            NominalDelta(months=1) + NominalDelta(months=2),
+            NominalDelta(months=3),
+        )
+
+    def test_sub(self):
+        self.assertEqual(
+            NominalDelta(months=1) - NominalDelta(days=2),
+            NominalDelta(months=1, days=-2),
+        )
+        self.assertEqual(
+            NominalDelta(months=1) - NominalDelta(months=2),
+            NominalDelta(months=-1),
+        )
+
+    def test_neg(self):
+        self.assertEqual(
+            -NominalDelta(months=1, days=-2),
+            NominalDelta(months=-1, days=2),
+        )
+
+    def test_mul(self):
+        self.assertEqual(
+            NominalDelta(months=1, days=-2) * 2,
+            NominalDelta(months=2, days=-4),
+        )
+
+    def test_rmul(self):
+        self.assertEqual(
+            2 * NominalDelta(months=1, days=-2),
+            NominalDelta(months=2, days=-4),
+        )
+
+    def test_radd_str_type_error(self):
+        with self.assertRaises(TypeError):
+            'asd' + NominalDelta(month=1)
+
+        with self.assertRaises(TypeError):
+            NominalDelta(month=1) + 'asd'
+
+    def test_radd_date(self):
+        self.assertEqual(
+            date(1970, 1, 30) + NominalDelta(months=1),
+            date(1970, 2, 28),
+        )
+
+    def test_radd_datetime(self):
+        self.assertEqual(
+            datetime(1970, 1, 30, 13) + NominalDelta(months=1),
+            datetime(1970, 2, 28, 13),
+        )
+        self.assertEqual(
+            datetime(1970, 1, 30, 13) + NominalDelta(hours=2),
+            datetime(1970, 1, 30, 15),
+        )
+        self.assertEqual(
+            datetime(1970, 1, 30, 13) + NominalDelta(hours=24),
+            datetime(1970, 1, 31, 13),
+        )
+        self.assertEqual(
+            datetime(1970, 1, 30, 13) + NominalDelta(seconds=80),
+            datetime(1970, 1, 30, 13, 1, 20),
+        )
+
+    def test_radd_datetime_dst(self):
+        tz = ZoneInfo('Europe/Berlin')
+        self.assertEqual(
+            datetime(2019, 3, 31, 1, 59, tzinfo=tz) + NominalDelta(minutes=2),
+            datetime(2019, 3, 31, 3, 1, tzinfo=tz),
+        )
+        self.assertEqual(
+            datetime(2019, 10, 27, 2, 59, tzinfo=tz) + NominalDelta(minutes=2),
+            datetime(2019, 10, 27, 2, 1, fold=True, tzinfo=tz),
+        )
+
+    def test_rsub_date(self):
+        self.assertEqual(
+            date(1970, 1, 30) - NominalDelta(days=5),
+            date(1970, 1, 25),
+        )
+
+    def test_rsub_datetime(self):
+        self.assertEqual(
+            datetime(1970, 1, 30, 13) - NominalDelta(days=1),
+            datetime(1970, 1, 29, 13),
+        )
+        self.assertEqual(
+            datetime(1970, 1, 30, 13) - NominalDelta(hours=2),
+            datetime(1970, 1, 30, 11),
+        )
+        self.assertEqual(
+            datetime(1970, 1, 30, 13) - NominalDelta(hours=24),
+            datetime(1970, 1, 29, 13),
+        )
+        self.assertEqual(
+            datetime(1970, 1, 30, 13) - NominalDelta(seconds=80),
+            datetime(1970, 1, 30, 12, 58, 40),
+        )
+
+    def test_rsub_datetime_dst(self):
+        tz = ZoneInfo('Europe/Berlin')
+        self.assertEqual(
+            datetime(2019, 3, 31, 3, 1, tzinfo=tz) - NominalDelta(minutes=2),
+            datetime(2019, 3, 31, 1, 59, tzinfo=tz),
+        )
+        self.assertEqual(
+            datetime(2019, 10, 27, 2, 1, fold=True, tzinfo=tz)
+            - NominalDelta(minutes=2),
+            datetime(2019, 10, 27, 2, 59, tzinfo=tz),
+        )
+
+    def test_diff_date(self):
+        self.assertEqual(
+            NominalDelta.diff(date(1970, 1, 15), date(1970, 2, 15)),
+            NominalDelta(months=1),
+        )
+        self.assertEqual(
+            NominalDelta.diff(date(1970, 2, 15), date(1970, 1, 15)),
+            NominalDelta(months=-1),
+        )
+        self.assertEqual(
+            NominalDelta.diff(date(1000, 1, 1), date(2000, 1, 1)),
+            NominalDelta(months=12_000),
+        )
+        self.assertEqual(
+            NominalDelta.diff(date(1970, 1, 15), date(1970, 1, 16)),
+            NominalDelta(days=1),
+        )
+
+    def test_diff_datetime(self):
+        self.assertEqual(
+            NominalDelta.diff(datetime(1970, 1, 30, 13), datetime(1970, 2, 28, 13)),
+            NominalDelta(months=1),
+        )
+        self.assertEqual(
+            NominalDelta.diff(datetime(1970, 1, 30, 13), datetime(1970, 1, 31, 13)),
+            NominalDelta(days=1),
+        )
+        self.assertEqual(
+            NominalDelta.diff(datetime(1970, 1, 30, 13), datetime(1970, 1, 30, 15)),
+            NominalDelta(hours=2),
+        )
+        self.assertEqual(
+            NominalDelta.diff(
+                datetime(1970, 1, 30, 13),
+                datetime(1970, 1, 30, 13, 1, 20),
+            ),
+            NominalDelta(seconds=80),
+        )
